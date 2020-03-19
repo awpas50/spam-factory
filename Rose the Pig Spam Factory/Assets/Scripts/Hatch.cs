b0b6c059c5bfbef73c32473e9e63d4efdb2c54fc
awpas50 <awper9794@gmail.com>
2020-03-19 17:07:12 +1000

    Added LWRP light

--- Rose the Pig Spam Factory/Assets/Scripts/Hatch.cs
+++ Rose the Pig Spam Factory/Assets/Scripts/Hatch.cs
@@ -6,22 +6,40 @@ public class Hatch : MonoBehaviour
 {
     public Animator animator;
     public bool debugTest = false;
+
+    public float freezeTime;
+    public float hatchOpenTime;
+    public float hatchCloseTime;
+
+    private bool isFreezed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(HatchInteraction());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!debugTest)
-        {
-            CloseGate();
-        }
-        if (debugTest)
+        //if (!debugTest)
+        //{
+        //    CloseGate();
+        //}
+        //if (debugTest)
+        //{
+        //    OpenGate();
+        //}
+    }
+
+    IEnumerator HatchInteraction()
+    {
+        yield return new WaitForSeconds(freezeTime);
+        while (true)
         {
             OpenGate();
+            yield return new WaitForSeconds(hatchOpenTime);
+            CloseGate();
+            yield return new WaitForSeconds(hatchCloseTime);
         }
     }
 
