c757bb93fe46ae612f153c4d538c0bc4539b4682
awpas50 <awper9794@gmail.com>
2020-03-19 17:16:04 +1000

    Added LWRP light

---
+++ Rose the Pig Spam Factory/Assets/Scripts/BarrierLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierLight : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.LWRP.Light2D neonLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        neonLight = transform.GetChild(0).GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
